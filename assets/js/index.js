71c70018835d084586b1add641c16a380d083949
Ali Tarichi <tarichiali@gmail.com>
2024-01-24 20:52:13 +0300

    services fixed

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,25 @@
 /*=============== SERVICES MODAL ===============*/
+const modal = document.querySelectorAll('.services__modal'),
+modalButton = document.querySelectorAll('.services__button'),
+modalClose = document.querySelectorAll('.services__modal-close')
 
+let activeModal = (modalClick) => {
+    modal[modalClick].classList.add('active-modal')
+}
 
+modalButton.forEach((modalButton, i) => {
+    modalButton.addEventListener('click', () => {
+        activeModal(i)
+    })
+})
+
+modalClose.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modal.forEach((modalRemove) => {
+            modalRemove.classList.remove('active-modal')
+        })
+    })
+})
 /*=============== SWIPER TESTIMONIAL ===============*/
 
 

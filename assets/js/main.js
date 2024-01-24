03c7cadc3c04bface1b5a86c1aa9c1fee4641f8b
Ali Tarichi <tarichiali@gmail.com>
2024-01-24 16:26:45 +0300

    works until here / header is done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,22 @@
 /*=============== SHOW MENU ===============*/
 
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/* Menu show */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* Menu hidden */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 

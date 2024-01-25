2abd057b72c527153be61b976308b6da6b139f72
Ali Tarichi <tarichiali@gmail.com>
2024-01-25 20:19:54 +0300

    scroll up added and footer is done as well

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,6 +20,7 @@ modalClose.forEach((modalClose) => {
         })
     })
 })
+
 /*=============== SWIPER TESTIMONIAL ===============*/
 const swiperTestimonial = new Swiper('.testimonial__swiper', {
     loop: true,
@@ -34,3 +35,10 @@ const swiperTestimonial = new Swiper('.testimonial__swiper', {
   });
 
 /*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)

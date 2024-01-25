38725d7972705c65dd0a331f649d22440d3d2535
Ali Tarichi <tarichiali@gmail.com>
2024-01-25 13:20:14 +0300

    testimonial done . testimonial pagination works as well

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,6 +21,16 @@ modalClose.forEach((modalClose) => {
     })
 })
 /*=============== SWIPER TESTIMONIAL ===============*/
+const swiperTestimonial = new Swiper('.testimonial__swiper', {
+    loop: true,
+    spaceBetween: 32,
+    grabCursor: true,
 
+    pagination: {
+      el: '.swiper-pagination',
+      dynamicBullets: true,
+      clickable: true,
+    },
+  });
 
 /*=============== SHOW SCROLL UP ===============*/ 
